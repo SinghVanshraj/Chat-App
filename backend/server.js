2ba56a12712d5c02b5d103256bb60aec8ff0d425
SinghVanshraj <vanshrajsingh28jul2004@gmail.com>
2026-06-01 18:12:26 +0530

    full project

--- backend/server.js
+++ backend/server.js
@@ -149,5 +149,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
-    server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+    server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
